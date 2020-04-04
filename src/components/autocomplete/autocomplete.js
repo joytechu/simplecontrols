91d0c98cce7a508c18f9269b959d1b2aa37e019a
kevin joy <kevin.joy@joytech.uk.com>
2020-04-04 18:23:51 +0100

    changes

---
+++ src/components/autocomplete/autocomplete.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Autocomplete() {
+  return (
+      <div className="autocomplete">
+        <input type="text" />
+      </div>
+  );
+}
+
+export default Autocomplete;
