b3bfac05ebfc65755a2bf136dbd7838f053ef3f4
kevin joy <kevin.joy@joytech.uk.com>
2020-04-12 19:16:41 +0100

    changes

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import "~react-html-components/"
-import Autocomplete from "react-html-components"
+import Autocomplete from "simplecontrols"
 
 function App() {
   return (
     <div className="App">
-      <Autocomplete />
+      <Autocomplete 
+        onFetchData={(val,cb) => setTimeout(() => cb( [{ DisplayText: "llo"}]), 1000)} 
+        onSelectedResult={(item) => { console.log(item)}}
+      />
     </div>
   );
 }
